218e1421a3dc215a1674ee1669831a16d2503d86
Vathiya Rezky Aliefasyah <79706184+vathxra@users.noreply.github.com>
2021-07-30 07:07:17 +0700

    Add files via upload

---
+++ Q4.java
@@ -0,0 +1,6 @@
+public class Q4 {
+    public static void main(String args[]) {
+    StringBuffer name = new StringBuffer("Vathiya Rezky Aliefasyah");
+     System.out.println(name.reverse());
+    }
+}
